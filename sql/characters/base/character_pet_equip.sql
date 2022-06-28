4ab74eba6c9a3686823502186157b8893441979c
Lenzork <83922068+Lenzork@users.noreply.github.com>
2022-06-28 12:32:47 -0700

    Upload Module
    Initial upload of the Module

---
+++ sql/characters/base/character_pet_equip.sql
@@ -0,0 +1,29 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server Version:               8.0.28 - MySQL Community Server - GPL
+-- Server Betriebssystem:        Win64
+-- HeidiSQL Version:             11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Exportiere Datenbank Struktur für acore_characters
+CREATE DATABASE IF NOT EXISTS `acore_characters` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `acore_characters`;
+
+-- Exportiere Daten aus Tabelle acore_characters.character_pet_equip: ~0 rows (ungefähr)
+/*!40000 ALTER TABLE `character_pet_equip` DISABLE KEYS */;
+INSERT INTO `character_pet_equip` (`ID`, `guid`, `equipmentId`) VALUES
+	(0, 17, 2);
+/*!40000 ALTER TABLE `character_pet_equip` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
---
+++ src/petequip.cpp
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
+ */
+
+#include "ScriptMgr.h"
+#include "Player.h"
+#include "Config.h"
+#include "Chat.h"
+#include "Pet.h"
+
+// Pet Equipment Module by Lenzork
+// Have fun with it!
+class PetEquip : public PetScript
+{
+public:
+    PetEquip() : PetScript("PetEquip") { }
+
+    void OnPetAddToWorld(Pet* pet) {
+        if(sConfigMgr->GetOption<bool>("PetEquip.Enable", false)){ // Check if Module is activated
+            int ownerGUID = pet->GetOwnerGUID().GetCounter();
+            QueryResult result = CharacterDatabase.Query("SELECT `equipmentId` FROM `character_pet_equip` WHERE guid = {}", ownerGUID);
+            if (result)
+            {
+                do {
+                    pet->SetCurrentEquipmentId((*result)[0].Get<int>());
+                    pet->LoadEquipment((*result)[0].Get<int>(), true);
+                } while (result->NextRow());
+            }
+        }
+    }
+};
+
+class PetEquipCommand : public CommandScript
+{
+public:
+    PetEquipCommand() : CommandScript("PetEquipCommand") { }
+
+    std::vector<ChatCommand> GetCommands() const override
+    {
+        static std::vector<ChatCommand> commandTable =
+        {
+            { "petequip",   SEC_PLAYER,      false,            HandlePetEquipCommand,         "Enter a number after it to change the Player Pets equipment Table if there are some. Helpful for example for Warlocks!"}
+        };
+
+        return commandTable;
+    }
+
+    static bool HandlePetEquipCommand(ChatHandler* handler, int args)
+    {
+        if(sConfigMgr->GetOption<bool>("PetEquip.Enable", false)){ // Check if Module is activated
+            if (!args)
+            {
+                handler->SendSysMessage(LANG_CMD_SYNTAX);
+                handler->SetSentErrorMessage(true);
+                return false;
+            }
+
+            if (args) {
+                if(handler->GetPlayer()->GetPet()){
+                    Pet* playerPet = handler->GetPlayer()->GetPet();
+                    uint32 ownerGUID = handler->GetPlayer()->GetGUID().GetCounter();
+                    playerPet->SetCurrentEquipmentId(args);
+                    CharacterDatabase.Execute("UPDATE character_pet_equip SET equipmentId = {} WHERE guid={}", args, ownerGUID);
+                    playerPet->LoadEquipment(args, true);
+                    playerPet->Say("A new Weapon? Hmpf...", LANG_UNIVERSAL);
+                    handler->PSendSysMessage("Successfully set the Pet Equipment ID to " + args);
+                    handler->PSendSysMessage(args);
+                }
+                else {
+                    handler->PSendSysMessage("You do not have a Pet!");
+                }
+            }
+        }
+        return true;
+    }
+};
+
+// Add all scripts in one
+void AddPetEquipScripts()
+{
+    new PetEquip();
+    new PetEquipCommand();
+}
